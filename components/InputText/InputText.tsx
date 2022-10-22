f6d7e035368c91b9211ab8a2ade64afee19114fc
Lety <leticiapistarelli@gmail.com>
2022-10-22 15:44:48 -0300

    Modique el componente input

---
+++ components/InputText/InputText.tsx
@@ -0,0 +1,88 @@
+import { Box, IconButton, TextField } from "@mui/material";
+import { FC, useState } from "react";
+import { useController, useFormContext } from "react-hook-form";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+interface Props {
+  name: string;
+  label?: string;
+  defaultValue?: string;
+  type?: string;
+  regex?: RegExp;
+  maxLength?: number;
+}
+const InputText: FC<Props> = ({
+  name,
+  label,
+  type,
+  defaultValue,
+  regex,
+  maxLength,
+}) => {
+  const { control } = useFormContext();
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const {
+    field: { onChange, value, ref },
+    formState: { errors },
+  } = useController<Record<string, string>>({
+    name: name,
+    control,
+    defaultValue,
+  });
+
+  const inputValue = (value: any) => {
+    if (regex) {
+      return value.match(regex) ? value : "";
+    } else {
+      return value;
+    }
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <Box
+      mb={2}
+      sx={{
+        position: "relative",
+        paddingY: "5px",
+        display: "flex",
+        alignItems: "center",
+      }}>
+      <TextField
+        onChange={onChange}
+        value={inputValue(value)}
+        label={label}
+        name={name}
+        inputRef={ref}
+        type={showPassword ? "text" : type}
+        inputProps={{ maxLength: maxLength }}
+        fullWidth
+        error={!!errors[name]}
+        helperText={`${errors[name]?.message || ""}`}
+        sx={{
+          backgroundColor: "#fff",
+        }}
+      />
+      {type === "password" && (
+        <IconButton
+          aria-label="toggle password visibility"
+          onClick={handleClickShowPassword}
+          sx={{
+            position: "absolute",
+            right: "2px",
+          }}>
+          {showPassword ? (
+            <VisibilityOffOutlinedIcon fontSize="small" />
+          ) : (
+            <VisibilityOutlinedIcon fontSize="small" />
+          )}
+        </IconButton>
+      )}
+    </Box>
+  );
+};
+export default InputText;
