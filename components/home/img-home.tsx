3680d51a19eb848e34eb155ac4220ce5733476a6
Lety <leticiapistarelli@gmail.com>
2022-10-26 23:13:02 -0300

    Mofico estilos img-home.tsx

--- components/home/img-home.tsx
+++ components/home/img-home.tsx
@@ -4,32 +4,6 @@ import Typography from "@mui/material/Typography";
 import styled from "styled-components";
 import { IHomeData } from "types/IHomeData.type";
 
-const Content = styled.div<Image>`
-  background-image: url(${(props) => props.image});
-  height: 90vh;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position-y: -48px;
-  position: relative;
-`;
-
-const BoxGreen = styled.div`
-  width: 100%;
-  height: 148px;
-  background-color: #C1FD35;
-  border-radius: 30px 30px 0px 0px;
-  position: absolute;
-  bottom: 0;
-`;
-
-const Space = styled.div`
-    height: 2.5px;
-    width: 100%;
-    background-color: #C1FD35;
-    margin: 10px 0;
-`;
-
 type Image = {
   image: string;
 };
@@ -52,18 +26,17 @@ const HomeImage: FC<Props> = ({ image, data }) => {
             {data[0].text}
           </Typography>
         </Box>
-        <BoxGreen/>
+        <BoxGreen />
         <Box
           sx={{
             display: "flex",
-            flexDirection: { sm: "row" } ,
+            flexDirection: { sm: "row" },
             flexWrap: "wrap",
             justifyContent: "space-evenly",
             position: "absolute",
             bottom: "10%",
             width: " 100%",
           }}>
-            
           <Box
             sx={{
               bgcolor: "#ffffff",
@@ -71,9 +44,17 @@ const HomeImage: FC<Props> = ({ image, data }) => {
               width: "500px",
               padding: 3,
             }}>
-            <Typography variant="h4">{data[1].title}</Typography>
-            <Space/>
-            <Typography>{data[1].text}</Typography>
+            <Typography fontWeight={700} variant="h4">
+              {data[1].title}
+            </Typography>
+            <Space />
+            <Typography
+              sx={{
+                fontSize: "1.2rem",
+                lineHeight: "1.7rem",
+              }}>
+              {data[1].text}
+            </Typography>
           </Box>
 
           <Box
@@ -81,11 +62,20 @@ const HomeImage: FC<Props> = ({ image, data }) => {
               bgcolor: "#ffffff",
               borderRadius: 3,
               width: "500px",
-              padding: 3,
+              padding: 4,
             }}>
-            <Typography variant="h4"> {data[2].title}</Typography>
-            <Space/>
-            <Typography>{data[2].text}</Typography>
+            <Typography fontWeight={700} variant="h4">
+              {" "}
+              {data[2].title}
+            </Typography>
+            <Space />
+            <Typography
+              sx={{
+                fontSize: "1.2rem",
+                lineHeight: "1.7rem",
+              }}>
+              {data[2].text}
+            </Typography>
           </Box>
         </Box>
       </Content>
@@ -94,3 +84,29 @@ const HomeImage: FC<Props> = ({ image, data }) => {
 };
 
 export default HomeImage;
+
+const Content = styled.div<Image>`
+  background-image: url(${(props) => props.image});
+  height: 90vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: -48px;
+  position: relative;
+`;
+
+const BoxGreen = styled.div`
+  width: 100%;
+  height: 148px;
+  background-color: #c1fd35;
+  border-radius: 30px 30px 0px 0px;
+  position: absolute;
+  bottom: 0;
+`;
+
+const Space = styled.div`
+  height: 2.5px;
+  width: 100%;
+  background-color: #c1fd35;
+  margin: 15px 0;
+`;
