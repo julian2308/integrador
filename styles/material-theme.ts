f9483549a7760eefa684b229c4bb9c8b559da74c
Lety <leticiapistarelli@gmail.com>
2022-10-27 08:35:01 -0300

    Modifico estilos de input

--- styles/material-theme.ts
+++ styles/material-theme.ts
@@ -1,5 +1,11 @@
 import { createTheme } from "@mui/material";
 
+// declare module "@mui/material/TextField" {
+//   interface TextFieldPropsVariantOverrides {
+//     cardProduct: true;
+//   }
+// }
+
 export const theme = createTheme({
   palette: {
     primary: {
@@ -7,6 +13,47 @@ export const theme = createTheme({
     },
     secondary: {
       main: "#CECECE",
+    },
+  },
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          color: "#fff",
+
+          //
+          "& .MuiOutlinedInput-notchedOutline:hover": {
+            borderWidth: 1,
+            borderColor: "#ffffff",
+          },
+          "&:hover $notchedOutline": {
+            borderColor: "#fff",
+          },
+          "&:hover": {
+            borderColor: "#fff",
+          },
+          //
+        },
+
+        notchedOutline: {
+          borderColor: "#fff",
+          color: "#fff",
+          //
+          "&:hover": {
+            borderColor: "#fff",
+          },
+          //
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        outlined: {
+          // "&$focused": {
+          color: "#fff",
+          // },
+        },
+      },
     },
   },
 });
